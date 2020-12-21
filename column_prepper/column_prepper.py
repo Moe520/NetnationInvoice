7377bf60c3008c481f3325d76672eedd99d3ffd0
Moe <Moe520@users.noreply.github.com>
2020-12-21 10:01:43 -0800

    More docstrings for some abstract classes

--- column_prepper/column_prepper.py
+++ column_prepper/column_prepper.py
@@ -3,4 +3,11 @@ class ColumnPrepper:
         self.prepping_strategy = prep_column_strategy
 
     def prep_column(self, data_ref,type_map,reduction_map):
+        """
+        Will transform a column based on a given strategy
+        :param data_ref: Reference to a pandas dataframe
+        :param type_map: used when mapping part number to item number
+        :param reduction_map: used to reduce quantities for certain items
+        :return:
+        """
         self.prepping_strategy.prep_column(self.prepping_strategy, data_ref, type_map,reduction_map)
--- skippable_row_dropper/skippable_row_dropper.py
+++ skippable_row_dropper/skippable_row_dropper.py
@@ -6,4 +6,9 @@ class SkippableRowDropper:
         self.dropping_strategy = drop_rows_strategy
 
     def drop_bad_rows(self, data_ref, error_logger):
+        """
+        :param data_ref: Reference to a pandas dataframe
+        :param error_logger: Will be used to report bad rows to a log file
+        :return:
+        """
         self.dropping_strategy.drop_bad_rows(self.dropping_strategy, data_ref, error_logger)
